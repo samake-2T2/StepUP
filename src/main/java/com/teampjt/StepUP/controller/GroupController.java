4abd35cf24e56503d9a81be1030d1ecc46017fea
hk <sky210hk@gmail.com>
2022-04-20 18:01:42 +0900

    hk20220420

--- src/main/java/com/teampjt/StepUP/controller/GroupController.java
+++ src/main/java/com/teampjt/StepUP/controller/GroupController.java
@@ -1,12 +1,24 @@
 package com.teampjt.StepUP.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.teampjt.StepUP.command.UserVO;
+import com.teampjt.StepUP.user.UserService;
+import com.teampjt.StepUP.util.Criteria;
+import com.teampjt.StepUP.util.PageVO;
 
 @Controller
 @RequestMapping("/group")
 public class GroupController {
+	
+	@Autowired
+	public UserService userService;
 
 	@GetMapping("/groupApplication")
 	public String groupApplication() {
@@ -23,8 +35,25 @@ public class GroupController {
 		return "group/groupRegist";
 	}
 	
+	//그룹 신청인 목록 조회(그룹장이 확인)
 	@GetMapping("/groupRegList")
-	public String groupRegList() {
+	public String groupRegList(Model model, Criteria cri) {
+		
+		//페이징 처리
+		ArrayList<UserVO> list = userService.getUserList(cri);
+		int total = userService.getUserTotal();
+		
+		PageVO pageVO = new PageVO(cri, total);
+		
+		//데이터 저장
+		model.addAttribute("userlist", list);
+		
+		//페이지네이션 저장
+		model.addAttribute("pageVO", pageVO);
+		
+		
+		
+		
 		return "group/groupRegList";
 	}
 	
--- src/main/java/com/teampjt/StepUP/user/UserServiceImpl.java
+++ src/main/java/com/teampjt/StepUP/user/UserServiceImpl.java
@@ -1,12 +1,29 @@
 package com.teampjt.StepUP.user;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.teampjt.StepUP.command.UserVO;
+import com.teampjt.StepUP.util.Criteria;
 
 @Service("userService")
 public class UserServiceImpl implements UserService {
 	
 	@Autowired
 	private UserMapper userMapper;
+
+	@Override
+	public ArrayList<UserVO> getUserList(Criteria cri) {
+
+		return userMapper.getUserList(cri);
+	}
+
+	@Override
+	public int getUserTotal() {
+
+		return userMapper.getUserTotal();
+	}
 
 }
