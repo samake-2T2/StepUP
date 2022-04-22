56bb2ea7d560660e3c3c12ea3a627db61327c037
jung <dnkkslr412@gmail.com>
2022-04-22 17:53:42 +0900

    건중입니다

--- src/main/java/com/teampjt/StepUP/group/GroupService.java
+++ src/main/java/com/teampjt/StepUP/group/GroupService.java
@@ -1,5 +1,13 @@
 package com.teampjt.StepUP.group;
 
+import java.util.ArrayList;
+
+import com.teampjt.StepUP.command.GroupNoticeVO;
+
 public interface GroupService {
 
+	public int noticeRegist(GroupNoticeVO gnVO); //공지등록
+	public ArrayList<GroupNoticeVO> getNoticeList(); //공지목록
+	public GroupNoticeVO getNoticeDetail(int groupnotice_no); //공지상세보기
+	public int updatecount(int groupnotice_no); //조회수
 }
