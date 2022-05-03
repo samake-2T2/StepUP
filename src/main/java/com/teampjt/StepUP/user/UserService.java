626b5bfa7ac5b51bba84903b17ecb6951f728b39
young71 <oyg961028@gamil.com>
2022-05-03 17:53:55 +0900

    절망.. 사진

--- src/main/java/com/teampjt/StepUP/user/UserService.java
+++ src/main/java/com/teampjt/StepUP/user/UserService.java
@@ -7,14 +7,14 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.teampjt.StepUP.command.GroupDetailCommentVO;
 import com.teampjt.StepUP.command.UserVO;
+import com.teampjt.StepUP.command.userUploadVO;
 import com.teampjt.StepUP.util.Criteria;
 
 public interface UserService {
 	
-	public int regist(UserVO vo);
+	public int userRegist(UserVO vo); //회원가입
 	
-	public int userJoin(UserVO vo);
-	
+	public int registFile(userUploadVO vo, MultipartFile f); //파일 등록
 
 	public int userDelete(int user_no);
 	//그룹 신청인 목록 조회 메서드
